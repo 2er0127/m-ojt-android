dcd9ac167ef05f803ca35d673245ac07a3438a6b
2er0ooz <2er0.oz1228@gmail.com>
2024-10-04 15:43:19 +0900

    feat: fin IsDebugging

--- app/src/main/java/com/zero/vulnlab/DebugActivity.kt
+++ app/src/main/java/com/zero/vulnlab/DebugActivity.kt
@@ -23,19 +23,6 @@ class DebugActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_is_debugging)
-
-        val statusTextView: TextView = findViewById(R.id.debugIsStatus)
-        val checkDebuggingButton: Button = findViewById(R.id.checkDebuggingButton)
-
-        checkDebuggingButton.setOnClickListener {
-            if (isDebugging()) {
-                statusTextView.text = "디버깅 활성화"
-                makeText(this, "디버깅이 감지되었습니다.", LENGTH_SHORT).show()
-            } else {
-                statusTextView.text = "디버깅 비활성화"
-                makeText(this, "디버깅이 감지되지 않았습니다.", LENGTH_SHORT).show()
-            }
-        }
     }
 
     private fun isDebugging(): Boolean {
